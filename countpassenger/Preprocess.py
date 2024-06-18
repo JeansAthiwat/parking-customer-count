de036e1d6d55edad67359e3b31800db43b1a3361
JeansAthiwat <athiwat5146@gmail.com>
2024-06-18 16:23:12 +0700

    feat: preprocessor todo

--- countpassenger/Preprocess.py
+++ countpassenger/Preprocess.py
@@ -3,26 +3,50 @@ import numpy as np
 import os
 from countpassenger.Config import conf
 
-drop_label_vehicle = ['car_brand_model',
-              'vehicle_type_model',
-              'plate_number_definition',
-              'plate_representative_vehicle_image_name',
-              'parent_image_name',
-              'full_image_names',
-              'timestamp',
-              ]
-
-important_label_vehicle = [
-    'timestamp_precise', 
+drop_label_vehicle = [
+    "car_brand_model",
+    "vehicle_type_model",
+    "plate_number_definition",
+    "plate_representative_vehicle_image_name",
+    "parent_image_name",
+    "full_image_names",
+    "timestamp",
 ]
 
-def df_clean_vehicle(df_vehicle: pd.DataFrame, drop_na:list = ['plate_number'] ,drop_label: list = None):
+IMPORTANT_LABEL_VEHICLE = [
+    "timestamp_precise",
+    "camera-cleaned",
+    "plate_number",
+    "lifetime",
+    "vehicle_type",
+    "xmin",
+    "xmax",
+    "ymin",
+    "ymax",
+]
+
+
+def df_clean_vehicle(
+    df_vehicle: pd.DataFrame,
+    drop_na: list = ["plate_number"],
+    drop_label: list = None,
+    included_vehicle_type: list = ["van", "bus"],
+):
     filtered_vehicle = None
+    # drop N/A plate number
+    # filter to only van and bus
+    # clean up camera_name to camera_clean
     filtered_vehicle = df_vehicle.dropna(subset=drop_na)
-    #clean up camera_name to camera_clean
-    #drop N/A plate number
-    #filter to only van and bus
+    filtered_vehicle = filtered_vehicle[
+        filtered_vehicle["vehicle_type"].isin(included_vehicle_type)
+    ]
+    filtered_vehicle["camera_cleaned"] = filtered_vehicle["camera"].str.extract(
+        r"^(mbk-\d{2}-\d{2})"
+    )
+
+    # format timestamp_precise to datetime
     return filtered_vehicle
+
 
 # def df_clean_cross(df_customer: pd.DataFrame):
 #     return filtered_customer
