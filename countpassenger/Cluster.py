fd313fd2eae4c293e536ea739c76d23ecc7c3f0e
JeansAthiwat <athiwat5146@gmail.com>
2024-06-20 15:00:47 +0700

    add: cluster fn

--- countpassenger/Cluster.py
+++ countpassenger/Cluster.py
@@ -9,3 +9,27 @@ import os
 
 from sklearn.preprocessing import StandardScaler
 from sklearn.cluster import HDBSCAN
+
+
+def perform_cross_clustering(df_cross: pd.DataFrame, params: dict = conf.HDBSCAN_PARAMS):
+    cluster_cross = None
+
+    ################ WHY? ###########
+    # Normalize the x and y coordinates
+    scaler_xy = StandardScaler()
+    df_cross[["xmid_std", "ymid_std"]] = scaler_xy.fit_transform(df_cross[["xmid", "ymid"]])
+
+    # Normalize the timestamp_unix values
+    scaler_time = StandardScaler()
+    df_cross[["timestamp_unix_std"]] = scaler_time.fit_transform(df_cross[["timestamp_unix"]])
+    #################################
+    # Create a 3D array combining x, y, and timestamp_unix
+    data = df_cross[["xmid_std", "ymid_std", "timestamp_unix_std"]].values
+
+    # Define the HDBSCAN clustering algorithm
+
+    clusterer = HDBSCAN(**conf.HDBSCAN_PARAMS)  # Apply HDBSCAN
+    df_cross["cluster"] = clusterer.fit_predict(data)
+    # print(df_cross[df_cross["cluster"] == -1].to_string())
+
+    return df_cross, cluster_cross
