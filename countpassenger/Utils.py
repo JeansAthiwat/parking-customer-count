ed6200f1514bcb85d44738696d91a95a628d5fae
JeansAthiwat <athiwat5146@gmail.com>
2024-06-21 14:08:06 +0700

    good hyperparams for cluster both

--- countpassenger/Utils.py
+++ countpassenger/Utils.py
@@ -11,5 +11,5 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.cluster import HDBSCAN
 
 
-def get_df_in_timestamp_range(df: pd.DataFrame, start: np.int64 = 1714284000, stop: np.int64 = 1714288000):
-    return df[(df["timestamp_unix"] >= start) & (df["timestamp_unix_end"] <= stop)]
+def print_range(df: pd.DataFrame, start: np.int64 = 1714284000, stop: np.int64 = 1714288000):
+    print(df[(df["timestamp_unix"] >= start) & (df["timestamp_unix_end"] <= stop)].to_string())
