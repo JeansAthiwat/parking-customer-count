166b04776819fc3f1cc22b94ff18770ce189c8ce
JeansAthiwat <athiwat5146@gmail.com>
2024-06-21 08:08:46 +0700

    looks good

--- countpassenger/Cluster.py
+++ countpassenger/Cluster.py
@@ -10,7 +10,7 @@ import os
 from sklearn.preprocessing import StandardScaler
 from sklearn.cluster import HDBSCAN
 
-TIME_BIASED = 10
+TIME_BIASED = 7
 SPACE_BIASED = 800
 
 
@@ -33,20 +33,22 @@ def time_biased_distance2(point1, point2):
     return spatial_distance + time_distance
 
 
-def time_biased_distance1(point1, point2):
-    # Apply quadratic bias to spatial distance
+def time_biased_distance3(point1, point2):
     spatial_distance = np.sqrt((point1[0] - point2[0]) ** 2 + (point1[1] - point2[1]) ** 2)
-    if spatial_distance < SPACE_BIASED:
-        spatial_distance = 2 ** (spatial_distance / (SPACE_BIASED / 1.1))
-    # Quadratic distance for time difference
     time_difference = np.abs(point1[2] - point2[2])
-    if time_difference > TIME_BIASED:
-        time_distance = time_difference**2
+    if time_difference < TIME_BIASED:
+        time_difference = time_difference * 0.5
     else:
-        time_distance = time_difference * 0.2
+        time_difference = min(9999999, time_difference**2)
 
-    # Combine the distances
-    return spatial_distance + time_distance
+    return spatial_distance + time_difference * 10
+
+
+def time_biased_distance1(point1, point2):
+    spatial_distance = np.sqrt((point1[0] - point2[0]) ** 2 + (point1[1] - point2[1]) ** 2)
+    time_difference = np.abs(point1[2] - point2[2])
+
+    return spatial_distance + time_difference
 
 
 def perform_cross_clustering(df_cross: pd.DataFrame, params: dict = conf.HDBSCAN_PARAMS):
@@ -57,12 +59,29 @@ def perform_cross_clustering(df_cross: pd.DataFrame, params: dict = conf.HDBSCAN
     # scaler_time = StandardScaler()
     # df_cross[["timestamp_unix_std"]] = scaler_time.fit_transform(df_cross[["timestamp_unix"]])
     # data = df_cross[["xmid_std", "ymid_std", "timestamp_unix_std"]].values
+
+    # clusterer = HDBSCAN(
+    #     **conf.HDBSCAN_PARAMS, metric="euclidean"
+    # )  # Apply HDBSCAN time_biased_distance3
+    # clusters = clusterer.fit_predict(data)
+    # df_cross["cluster"] = clusters
+    # cluster_cross = create_cluster_df(
+    #     [
+    #         (x, y, t, res)
+    #         for (x, y, t), res in zip(df_cross[["xmid", "ymid", "timestamp_unix"]].values, clusters)
+    #     ]
+    # )
     # #################################
 
     data = df_cross[["xmid", "ymid", "timestamp_unix"]].values
-    clusterer = HDBSCAN(**conf.HDBSCAN_PARAMS, metric=time_biased_distance2)  # Apply HDBSCAN
+    clusterer = HDBSCAN(
+        **conf.HDBSCAN_PARAMS, metric="euclidean"
+    )  # Apply HDBSCAN time_biased_distance3
     clusters = clusterer.fit_predict(data)
     df_cross["cluster"] = clusters
+    # cluster_cross = create_cluster_df(
+    #     [(x, y, t.astype(np.int64), res.astype(np.int64)) for (x, y, t), res in zip(data, clusters)]
+    # )
     cluster_cross = create_cluster_df([(x, y, t, res) for (x, y, t), res in zip(data, clusters)])
 
     return df_cross, cluster_cross
@@ -92,6 +111,15 @@ def create_cluster_df(data):
     )
 
     df_cluster = grouped.rename(columns={"cluster": "cluster_id"})
+
+    df_cluster["timestamp_min"] = pd.to_datetime(
+        df_cluster["timestamp_unix_min"], unit="s"
+    ).dt.strftime("%Y-%m-%d %H:%M:%S")
+
+    df_cluster["timestamp_max"] = pd.to_datetime(
+        df_cluster["timestamp_unix_max"], unit="s"
+    ).dt.strftime("%Y-%m-%d %H:%M:%S")
+
     return df_cluster
 
 
@@ -108,12 +136,39 @@ def assign_cluster_to_vehicle(
         timestamp_max = cluster_row["timestamp_unix_max"]
         cluster_count = cluster_row["count"]
 
-        # Find all df_vehicle rows with timestamp_unix within the range
-        # TODO: get the nearest to timestamp_min 3 index thats df_vehicle["timestamp_unix"] <= timestamp_min
-        mask = (df_vehicle["timestamp_unix"] >= timestamp_min) & (
-            df_vehicle["timestamp_unix"] <= timestamp_max
-        )
-        matching_indices = df_vehicle.index[mask].tolist()
+        ####
+        # Find the first 2 indices <= timestamp_min   # 5minute
+        indices_before = df_vehicle.index[
+            (df_vehicle["timestamp_unix"] <= timestamp_min)
+            & (timestamp_min - df_vehicle["timestamp_unix"] < 60)
+        ].tolist()
+        if len(indices_before) > 2:
+            indices_before = indices_before[-2:]
+
+        # Find the next 2 indices >= timestamp_max
+        indices_after = df_vehicle.index[
+            (df_vehicle["timestamp_unix"] >= timestamp_max)
+            & (df_vehicle["timestamp_unix"] - timestamp_max < 60)
+        ].tolist()
+        if len(indices_after) > 2:
+            indices_after = indices_after[:2]
+
+        # Combine these indices
+        matching_indices = indices_before + indices_after
+
+        # Ensure no duplicate indices
+        matching_indices = list(set(matching_indices))
+        print(matching_indices)
+        ####
+
+        # # Find all df_vehicle rows with timestamp_unix within the range
+        # # TODO: change mask condition where df_vehicle["timestamp_unix"] should be the first 2 index that is <= timestamp_min or the next 2 index that is >= timestamp_max
+        # mask = (df_vehicle["timestamp_unix"] >= timestamp_min) & (
+        #     df_vehicle["timestamp_unix"] <= timestamp_max
+        # )
+        # # END OF TODO#
+
+        # matching_indices = df_vehicle.index[mask].tolist()
 
         if len(matching_indices) > 1:
             # Multiple matches: find the nearest based on (xmid, ymid) distance
--- countpassenger/Config.py
+++ countpassenger/Config.py
@@ -1,5 +1,8 @@
 import os
 import os.path as osp
+import math
+
+_TIME_BIAS = 6
 
 
 class Config:
@@ -15,19 +18,19 @@ class Config:
         # TODO: tune these
         self.HDBSCAN_PARAMS = dict(
             min_cluster_size=2,
-            min_samples=2,
-            cluster_selection_epsilon=0.0,
-            max_cluster_size=50,
+            min_samples=None,
+            cluster_selection_epsilon=0,  # math.sqrt(1440**2 + 2560**2) + _TIME_BIAS,
+            max_cluster_size=40,
             # specify yourown
             # metric="euclidean",
             # metric_params=None,
             alpha=1.0,
-            algorithm="auto",
-            leaf_size=40,
+            algorithm="brute",
+            leaf_size=1000,
             n_jobs=None,
-            cluster_selection_method="eom",
+            cluster_selection_method="leaf",
             allow_single_cluster=False,
-            store_centers=None,
+            store_centers="medoid",
             copy=False,
         )
 
@@ -64,6 +67,15 @@ class Config:
             "timestamp_precise",
             "xmid",
             "ymid",
+        ]
+
+        self.CLUSTER_INTEREST = [
+            "timestamp_min",
+            "timestamp_max",
+            "count",
+            "cluster_id",
+            "xmid_mean",
+            "ymid_mean",
         ]
 
         self.VEHICLE_INTEREST_NON_SNAPSHOT_CLUSTERED = [
