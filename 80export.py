309b11717f1abd439e8ef1b587d34a872b087e53
JeansAthiwat <athiwat5146@gmail.com>
2024-06-25 11:55:01 +0700

    lele sdf

--- 80export.py
+++ 80export.py
@@ -22,41 +22,43 @@ def concatenate_csv(files, output_path):
     print(f"Concatenated CSV written to {output_path}")
 
 
-# # Example usage
-# file_list = [
-#     "/home/jeans/internship/parking-customer-count/resources/processed/2024-04-29/count-passengers-mbk-14-11-vehicle-snapshot.csv",
-#     "/home/jeans/internship/parking-customer-count/resources/processed/2024-04-29/count-passengers-mbk-14-12-vehicle-snapshot.csv",
-#     "/home/jeans/internship/parking-customer-count/resources/processed/2024-04-29/count-passengers-mbk-14-13-vehicle-snapshot.csv",
-#     "/home/jeans/internship/parking-customer-count/resources/processed/2024-04-29/count-passengers-mbk-14-14-vehicle-snapshot.csv",
-# ]
-# output_file = "/home/jeans/internship/parking-customer-count/resources/processed/2024-04-29_count-passengers-mbk-tourist-vehicle-object-snapshot.csv"
+file_list = [
+    "/home/jeans/internship/parking-customer-count/resources/processed/2024-04-29/count-passengers-mbk-14-11-vehicle-snapshot.csv",
+    "/home/jeans/internship/parking-customer-count/resources/processed/2024-04-29/count-passengers-mbk-14-12-vehicle-snapshot.csv",
+    "/home/jeans/internship/parking-customer-count/resources/processed/2024-04-29/count-passengers-mbk-14-13-vehicle-snapshot.csv",
+    "/home/jeans/internship/parking-customer-count/resources/processed/2024-04-29/count-passengers-mbk-14-14-vehicle-snapshot.csv",
+]
+output_file = "/home/jeans/internship/parking-customer-count/resources/processed/2024-04-29_count-passengers-mbk-tourist-vehicle-object-snapshot.csv"
+concatenate_csv(file_list, output_file)
 
-# # Example usage
-# file_list = [
-#     "/home/jeans/internship/parking-customer-count/resources/processed/2024-04-29/count-passengers-mbk-14-11-vehicle.csv",
-#     "/home/jeans/internship/parking-customer-count/resources/processed/2024-04-29/count-passengers-mbk-14-12-vehicle.csv",
-#     "/home/jeans/internship/parking-customer-count/resources/processed/2024-04-29/count-passengers-mbk-14-13-vehicle.csv",
-#     "/home/jeans/internship/parking-customer-count/resources/processed/2024-04-29/count-passengers-mbk-14-14-vehicle.csv",
-# ]
-# output_file = "/home/jeans/internship/parking-customer-count/resources/processed/2024-04-29_count-passengers-mbk-tourist-vehicle-object.csv"
 
-# # Example usage
-# file_list = [
-#     "/home/jeans/internship/parking-customer-count/resources/processed/2024-04-28/count-passengers-mbk-14-11-vehicle-snapshot.csv",
-#     "/home/jeans/internship/parking-customer-count/resources/processed/2024-04-28/count-passengers-mbk-14-12-vehicle-snapshot.csv",
-#     "/home/jeans/internship/parking-customer-count/resources/processed/2024-04-28/count-passengers-mbk-14-13-vehicle-snapshot.csv",
-#     "/home/jeans/internship/parking-customer-count/resources/processed/2024-04-28/count-passengers-mbk-14-14-vehicle-snapshot.csv",
-# ]
-# output_file = "/home/jeans/internship/parking-customer-count/resources/processed/2024-04-28_count-passengers-mbk-tourist-vehicle-object-snapshot.csv"
+file_list = [
+    "/home/jeans/internship/parking-customer-count/resources/processed/2024-04-29/count-passengers-mbk-14-11-vehicle.csv",
+    "/home/jeans/internship/parking-customer-count/resources/processed/2024-04-29/count-passengers-mbk-14-12-vehicle.csv",
+    "/home/jeans/internship/parking-customer-count/resources/processed/2024-04-29/count-passengers-mbk-14-13-vehicle.csv",
+    "/home/jeans/internship/parking-customer-count/resources/processed/2024-04-29/count-passengers-mbk-14-14-vehicle.csv",
+]
+output_file = "/home/jeans/internship/parking-customer-count/resources/processed/2024-04-29_count-passengers-mbk-tourist-vehicle-object.csv"
+concatenate_csv(file_list, output_file)
 
-# # Example usage
-# file_list = [
-#     "/home/jeans/internship/parking-customer-count/resources/processed/2024-04-28/count-passengers-mbk-14-11-vehicle.csv",
-#     "/home/jeans/internship/parking-customer-count/resources/processed/2024-04-28/count-passengers-mbk-14-12-vehicle.csv",
-#     "/home/jeans/internship/parking-customer-count/resources/processed/2024-04-28/count-passengers-mbk-14-13-vehicle.csv",
-#     "/home/jeans/internship/parking-customer-count/resources/processed/2024-04-28/count-passengers-mbk-14-14-vehicle.csv",
-# ]
-# output_file = "/home/jeans/internship/parking-customer-count/resources/processed/2024-04-28_count-passengers-mbk-tourist-vehicle-object.csv"
+
+file_list = [
+    "/home/jeans/internship/parking-customer-count/resources/processed/2024-04-28/count-passengers-mbk-14-11-vehicle-snapshot.csv",
+    "/home/jeans/internship/parking-customer-count/resources/processed/2024-04-28/count-passengers-mbk-14-12-vehicle-snapshot.csv",
+    "/home/jeans/internship/parking-customer-count/resources/processed/2024-04-28/count-passengers-mbk-14-13-vehicle-snapshot.csv",
+    "/home/jeans/internship/parking-customer-count/resources/processed/2024-04-28/count-passengers-mbk-14-14-vehicle-snapshot.csv",
+]
+output_file = "/home/jeans/internship/parking-customer-count/resources/processed/2024-04-28_count-passengers-mbk-tourist-vehicle-object-snapshot.csv"
+concatenate_csv(file_list, output_file)
+
+
+file_list = [
+    "/home/jeans/internship/parking-customer-count/resources/processed/2024-04-28/count-passengers-mbk-14-11-vehicle.csv",
+    "/home/jeans/internship/parking-customer-count/resources/processed/2024-04-28/count-passengers-mbk-14-12-vehicle.csv",
+    "/home/jeans/internship/parking-customer-count/resources/processed/2024-04-28/count-passengers-mbk-14-13-vehicle.csv",
+    "/home/jeans/internship/parking-customer-count/resources/processed/2024-04-28/count-passengers-mbk-14-14-vehicle.csv",
+]
+output_file = "/home/jeans/internship/parking-customer-count/resources/processed/2024-04-28_count-passengers-mbk-tourist-vehicle-object.csv"
 
 
 concatenate_csv(file_list, output_file)
