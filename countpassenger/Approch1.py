a8becc68260d1eaefebd193905fc69d09762b5c5
JeansAthiwat <athiwat5146@gmail.com>
2024-06-25 13:21:52 +0700

    add: park check for non snap

--- countpassenger/Approch1.py
+++ countpassenger/Approch1.py
@@ -7,6 +7,8 @@ import numpy as np
 import os.path as osp
 import os
 
+PARKED_CHECK_WINDOW = 120
+
 
 def match_cross_to_vehicle(df_cross, df_vehicle, start_padding: np.int64 = 5, stop_padding: np.int64 = 5):
 
@@ -19,9 +21,10 @@ def match_cross_to_vehicle(df_cross, df_vehicle, start_padding: np.int64 = 5, st
             df_vehicle_copy["timestamp_unix_end"] + stop_padding >= row["timestamp_unix"]
         )
 
-        mask_parked = (df_vehicle_copy["lifetime"] < 420) | (
-            (row["timestamp_unix"] <= df_vehicle_copy["timestamp_unix"] + 120)
-            | (df_vehicle_copy["timestamp_unix_end"] - 120 <= row["timestamp_unix"])
+        # if lifetime is more than 400sec it might be parking car check only the first 2 min and last 2 min
+        mask_parked = (df_vehicle_copy["lifetime"] < 400) | (
+            (row["timestamp_unix"] <= df_vehicle_copy["timestamp_unix"] + PARKED_CHECK_WINDOW)
+            | (df_vehicle_copy["timestamp_unix_end"] - PARKED_CHECK_WINDOW <= row["timestamp_unix"])
         )
 
         vehicle_rows = df_vehicle_copy[mask_time & mask_parked]
@@ -54,9 +57,9 @@ def match_reverse_to_vehicle(df_reverse, df_vehicle, start_padding: int = 5, sto
             df_vehicle_copy["timestamp_unix_end"] + stop_padding >= row["timestamp_unix"]
         )
 
-        mask_parked = (df_vehicle_copy["lifetime"] < 420) | (
-            (row["timestamp_unix"] <= df_vehicle_copy["timestamp_unix"] + 120)
-            | (df_vehicle_copy["timestamp_unix_end"] - 120 <= row["timestamp_unix"])
+        mask_parked = (df_vehicle_copy["lifetime"] < 400) | (
+            (row["timestamp_unix"] <= df_vehicle_copy["timestamp_unix"] + PARKED_CHECK_WINDOW)
+            | (df_vehicle_copy["timestamp_unix_end"] - PARKED_CHECK_WINDOW <= row["timestamp_unix"])
         )
 
         vehicle_rows = df_vehicle_copy[mask_time & mask_parked]
@@ -70,8 +73,6 @@ def match_reverse_to_vehicle(df_reverse, df_vehicle, start_padding: int = 5, sto
         )
         # # Find the index of the nearest vehicle
         nearest_index = distances.idxmin(axis=0)
-
-        # Increment the cross count and mark as used for the nearest vehicle
         df_vehicle_copy.loc[nearest_index, "reverse_count"] += 1
 
     return df_vehicle_copy
